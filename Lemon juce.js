0f319638b39868f00abc8028eefac9d1625673a3
Impulse-max <69639966+Impulse-max@users.noreply.github.com>
2020-09-19 16:33:45 +0530

    Add files via upload

---
+++ Lemon juce.js
@@ -0,0 +1,7 @@
+Cut the lemons lengthwise in half with a sharp knife.
+ Most people cut across the width of a lemon. Instead of doing that,
+  make a vertical cut down the center of each lemon. 
+  It makes the lemon halves easier to squeeze,
+ allowing you to get a little more juice out of them.
+ put it in a glass and add sugar and mix  well .
+ Enjoy.
